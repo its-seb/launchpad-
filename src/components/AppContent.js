a460cf2718f581e042f46cc8af11750611af005d
boonyeow <boon123321@gmail.com>
2021-12-08 21:38:05 +0800

    upload launchpad

--- src/components/AppContent.js
+++ src/components/AppContent.js
@@ -1,105 +1,76 @@
 import React, { Component } from "react";
-import ICONexConnection, {
-  estimateStepsforDeployment,
-} from "./utils/interact.js";
-import { fetchContractContent } from "./utils/fetchContractContent.js";
-import IconService from "icon-sdk-js";
 import { Container, Button, Row, Col, Card } from "react-bootstrap";
 import AppContentStyle from "./AppContent.module.css";
-const { IconConverter, IconBuilder } = IconService;
+import NewCollection from "./NewCollection.js";
 
-const AppContent = (props) => {
-  const connection = new ICONexConnection();
-
-  const handleDeployContract = async () => {
-    const contractContent = await fetchContractContent(
-      "https://raw.githubusercontent.com/OpenDevICON/token-score-factory/master/zips/owner_burnable_irc3.zip"
-    );
-    console.log(contractContent);
-    const walletAddress = "hxbd1375315c7732779edaa4c3903ffc9b93e82ca3"; //localStorage.getItem("USER_WALLET_ADDRESS");
-    const txParams = {
-      _name: "BILLY",
-      _symbol: "BILL",
-    };
-    const stepLimitInHex = await estimateStepsforDeployment(
-      walletAddress,
-      contractContent,
-      txParams
-    );
-
-    const stepLimit = IconConverter.toNumber(stepLimitInHex);
-    const txObj = new IconBuilder.DeployTransactionBuilder()
-      .nid("0x53") //0x53 for sejong - https://www.icondev.io/introduction/the-icon-network/testnet
-      .from(walletAddress)
-      .to("cx0000000000000000000000000000000000000000")
-      .stepLimit(IconConverter.toBigNumber(stepLimit))
-      .version(IconConverter.toBigNumber(3))
-      .timestamp(Date.now() * 1000)
-      .contentType("application/zip")
-      .content(contractContent)
-      .nonce(IconConverter.toBigNumber(1))
-      .params(txParams)
-      .build();
-
-    //console.log(txObj);
-    const payload = {
-      jsonrpc: "2.0",
-      method: "icx_sendTransaction",
-      id: 6639,
-      params: IconConverter.toRawTransaction(txObj),
-    };
-
-    try {
-      let rpcResponse = await connection.getJsonRpc(payload);
-      console.log(rpcResponse);
-      localStorage.setItem("CONTRACT_ADDRESS", rpcResponse["result"]);
-    } catch (e) {
-      console.log(e); //handle error here (e.g. user cancelled transaction; show message)
-    }
-  };
-
+const AppContent = () => {
   return (
     <div style={{ height: "calc(100vh - 91px)" }}>
       <Container style={{ padding: "25px" }}>
         <div className={AppContentStyle.sectionHeader}>
-          <span style={{ float: "left" }}>Collections</span>{" "}
+          <span style={{ float: "left" }}>Collections</span>
           <button className={AppContentStyle.btnAddContract}>
-            + Create Collection
+            New Collection
           </button>
         </div>
         <div style={{ padding: "0 25px" }}>
           <Row>
             <Col>
-              <Card>
-                <div style={{ display: "inline" }}>
+              <Card style={{ border: "1px solid #7030a0" }}>
+                <div
+                  style={{
+                    display: "flex",
+                    paddingLeft: "12px",
+                    paddingTop: "5px",
+                    paddingRight: "12px",
+                  }}
+                >
                   <span
                     style={{
                       fontWeight: "bold",
-                      fontSize: "1.2rem",
+                      fontSize: "1.5rem",
                       color: "#525252",
                     }}
                   >
                     Billy
                   </span>
-                  <span style={{ verticalAling: "sub" }}>BILL</span>
+                  <div style={{ width: "100%", textAlign: "right" }}>
+                    <label
+                      style={{
+                        verticalAlign: "sub",
+                        backgroundColor: "#7030a0",
+                        color: "white",
+                        paddingLeft: "8px",
+                        paddingRight: "8px",
+                        borderRadius: "0.5rem",
+                      }}
+                    >
+                      Bill
+                    </label>
+                  </div>
                 </div>
-                <span>$BILL</span>
-                <span>View Collection</span>
-                <span>Icon Tracker</span>
+                <span
+                  style={{
+                    padding: "5px 15px 10px 15px",
+                    textAlign: "left",
+                    fontWeight: "bold",
+                    color: "#525252",
+                    fontSize: "12px",
+                    whiteSpace: "nowrap",
+                    overflow: "hidden",
+                    textOverflow: "ellipsis",
+                  }}
+                >
+                  hxbd1375315c7732779edaa4c3903ffc9b93e82ca3
+                </span>
               </Card>
-            </Col>
-            <Col>
-              <Card>Hello</Card>
-            </Col>
-            <Col>
-              <Card>Hello</Card>
             </Col>
           </Row>
         </div>
       </Container>
-      <button id="deployContract" onClick={handleDeployContract}>
-        hello world
-      </button>
+      <Container style={{ padding: "0 25px" }}>
+        <NewCollection />
+      </Container>
     </div>
   );
 };
---
+++ src/components/NewCollection.js
@@ -0,0 +1,118 @@
+import React from "react";
+import { Button, Form } from "react-bootstrap";
+import "./global.css";
+import IconService from "icon-sdk-js";
+import { fetchContractContent } from "./utils/fetchContractContent.js";
+import ICONexConnection, {
+  estimateStepsforDeployment,
+} from "./utils/interact.js";
+
+const { IconConverter, IconBuilder } = IconService;
+const NewCollection = () => {
+  const connection = new ICONexConnection();
+  const handleDeployContract = async () => {
+    //check if user address is set:
+    if (localStorage.getItem("USER_WALLET_ADDRESS") == null) {
+      alert("Please connect to your wallet.");
+      return;
+    }
+
+    const contractContent = await fetchContractContent(
+      "https://raw.githubusercontent.com/OpenDevICON/token-score-factory/master/zips/owner_burnable_irc3.zip"
+    );
+    console.log(contractContent);
+    const walletAddress = localStorage.getItem("USER_WALLET_ADDRESS");
+
+    //remember to sanitize input-> allow alphanumeric only
+    const collectionName = document.getElementById("tbCollectionName").value;
+    const collectionSymbol =
+      document.getElementById("tbCollectionSymbol").value;
+
+    if (
+      collectionName == null ||
+      collectionName == "" ||
+      collectionSymbol == null ||
+      collectionSymbol == ""
+    ) {
+      alert("Please enter required fields");
+      return;
+    }
+
+    const txParams = {
+      _name: collectionName, //based on user input
+      _symbol: collectionSymbol, //based on user input
+    };
+    const stepLimitInHex = await estimateStepsforDeployment(
+      walletAddress,
+      contractContent,
+      txParams
+    );
+
+    const stepLimit = IconConverter.toNumber(stepLimitInHex);
+    const txObj = new IconBuilder.DeployTransactionBuilder()
+      .nid("0x53") //0x53 for sejong - https://www.icondev.io/introduction/the-icon-network/testnet
+      .from(walletAddress)
+      .to("cx0000000000000000000000000000000000000000") //constant
+      .stepLimit(IconConverter.toBigNumber(stepLimit))
+      .version(IconConverter.toBigNumber(3)) //constant
+      .timestamp(Date.now() * 1000) //constant
+      .contentType("application/zip") //.py score
+      .content(contractContent)
+      .nonce(IconConverter.toBigNumber(1))
+      .params(txParams)
+      .build();
+
+    //console.log(txObj);
+    const payload = {
+      jsonrpc: "2.0",
+      method: "icx_sendTransaction",
+      id: 6639,
+      params: IconConverter.toRawTransaction(txObj),
+    };
+
+    try {
+      let rpcResponse = await connection.getJsonRpc(payload);
+      console.log(rpcResponse);
+      localStorage.setItem("CONTRACT_ADDRESS", rpcResponse["result"]);
+    } catch (e) {
+      alert("User cancelled transaction");
+      console.log(e); //handle error here (e.g. user cancelled transaction; show message)
+    }
+  };
+
+  return (
+    <div style={{ padding: "0 25px" }}>
+      <Form.Floating className="mb-3">
+        <Form.Control
+          id="tbCollectionName"
+          type="text"
+          placeholder="Collection Name"
+          className="purpleTextbox"
+        />
+        <label htmlFor="tbCollectionName" style={{ color: "#525252" }}>
+          Collection Name
+        </label>
+      </Form.Floating>
+      <Form.Floating>
+        <Form.Control
+          id="tbCollectionSymbol"
+          type="text"
+          placeholder="Symbol"
+          className="purpleTextbox"
+        />
+        <label htmlFor="tbCollectionSymbol" style={{ color: "#525252" }}>
+          Symbol
+        </label>
+      </Form.Floating>
+      <Button
+        id="btnDeploy"
+        onClick={handleDeployContract}
+        style={{ marginTop: "15px", float: "right" }}
+      >
+        Deploy
+      </Button>
+    </div>
+  );
+};
+
+export default NewCollection;
