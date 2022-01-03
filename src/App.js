cd89c621ebbd636869398914499c2ee165f02fdc
boonyeow <boon123321@gmail.com>
2022-01-04 03:23:46 +0800

    HELLO TAILWIND

--- src/App.js
+++ src/App.js
@@ -11,155 +11,20 @@ import LaunchComponent from "./components/LaunchComponent.js";
 import Usergallery from "./components/Usergallery";
 import { Button } from "react-bootstrap";
 import ICONexConnection from "./components/utils/interact.js";
+import LandingComponent from "./components/LandingComponent";
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.db = new Dexie("contracts_deployed");
-    this.db.version(1).stores({
-      contracts: "contractAddress, walletAddress, name, symbol",
-    });
-    this.db.open().catch((error) => {
-      console.log("error", error);
-    });
-    this.state = {
-      walletAddress: null,
-      user_contract_address: null,
-      user_walletaddress: null,
-      user_contract_title: null,
-    };
   }
-
-  connection = new ICONexConnection();
-  componentDidMount() {
-    if (localStorage.getItem("USER_WALLET_ADDRESS") != null) {
-      document.getElementById("btnConnectWallet").innerHTML = "sign out";
-    }
-    const queryString = window.location.search;
-    const urlParams = new URLSearchParams(queryString);
-    this.setState({ user_contract_address: urlParams.get("collection") });
-    this.setState({ user_walletaddress: urlParams.get("user") });
-    this.setState({ user_contract_title: urlParams.get("title") });
-    console.log(this.state.user_contract_address)
-    console.log(this.state.user_walletaddress)
-
-  }
-
-  handleWalletEvent = async (event) => {
-    if (localStorage.getItem("USER_WALLET_ADDRESS") == null) {
-      //signing in
-      let _walletAddress = await this.connection.getWalletAddress();
-      if (_walletAddress != null) {
-        localStorage.setItem("USER_WALLET_ADDRESS", _walletAddress);
-        console.log(localStorage.getItem("USER_WALLET_ADDRESS"));
-        document.getElementById("btnConnectWallet").innerHTML = "sign out";
-        this.setState({ walletAddress: _walletAddress });
-        window.location.assign("/");
-      } else {
-        console.log("user cancelled login");
-      }
-    } else {
-      //signing out
-      localStorage.removeItem("USER_WALLET_ADDRESS");
-      localStorage.removeItem("SELECTED_CONTRACT_ADDRESS");
-      localStorage.removeItem("HAS_METAHASH");
-      document.getElementById("btnConnectWallet").innerHTML = "connect wallet";
-      window.location.assign("/");
-      this.setState({ walletAddress: null });
-    }
-
-    // console.log(localStorage.getItem("USER_WALLET_ADDRESS"));
-    // if (
-    //   document.getElementById("btnConnectWallet").innerHTML == "connect wallet"
-    // ) {
-    //   this.walletAddress = await this.connection.getWalletAddress();
-    //   if (this.walletAddress != "undefined" && this.walletAddress != null) {
-    //     localStorage.setItem("USER_WALLET_ADDRESS", this.walletAddress);
-    //     document.getElementById("btnConnectWallet").innerHTML = "sign out";
-    //     this.db.contracts.clear();
-    //     this.db.close();
-    //   }
-    // } else {
-    //   localStorage.removeItem("USER_WALLET_ADDRESS");
-    //   document.getElementById("btnConnectWallet").innerHTML = "connect wallet";
-    // }
-    event.preventDefault();
-  };
 
   render() {
     return (
-      <div style={{ backgroundColor: "#F2F2F2", height: "100%" }}>
-        {this.state.user_contract_address != null &&
-          this.state.user_walletaddress != null ? (
-          <Router>
-            <Routes>
-              <Route
-                exact
-                element={
-                  <Usergallery
-                    pageTitle="Usergallery"
-                    contract_address={this.state.user_contract_address}
-                    wallet_address={this.state.user_walletaddress}
-                    contract_name={this.state.user_contract_title}
-                  />
-                }
-                path="/Usergallery"
-              ></Route>
-            </Routes>
-          </Router>
-        ) : (
-          <div className="App">
-            <Router>
-              <div style={{ display: "flex", width: "100%", height: "100%" }}>
-                <SideNav />
-                <div className="pageContent">
-                  <div className="headerContent">
-                    <text
-                      className="pageTitle unselectable"
-                      id="_pageTitle"
-                    ></text>
-                    <Button
-                      id="btnConnectWallet"
-                      onClick={this.handleWalletEvent}
-                    >
-                      connect wallet
-                    </Button>
-                  </div>
-                  <div className="mainContent">
-                    <Routes>
-                      <Route
-                        exact
-                        element={
-                          <CollectionComponent
-                            pageTitle="Collection"
-                            walletAddress={this.state.walletAddress}
-                          />
-                        }
-                        path="/"
-                      ></Route>
-                      <Route
-                        exact
-                        element={
-                          <FileComponent
-                            pageTitle="Files"
-                            walletAddress={this.state.walletAddress}
-                          />
-                        }
-                        path="/files"
-                      ></Route>
-                      <Route
-                        exact
-                        element={<LaunchComponent pageTitle="Launch" />}
-                        path="/launch"
-                      ></Route>
-                    </Routes>
-                  </div>
-                </div>
-              </div>
-            </Router>
-          </div>
-        )}
-      </div>
+      <Router>
+        <Routes>
+          <Route exact element={<LandingComponent />} path="/"></Route>
+        </Routes>
+      </Router>
     );
   }
 }
